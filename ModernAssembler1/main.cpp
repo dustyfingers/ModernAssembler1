35055cea1f4616ee46a3c7ebf27ab29c727ae4f8
lou <dustyfingers01257@gmail.com>
2021-05-08 14:26:42 -0600

    learned about calling inline assembly in 32bit

--- ModernAssembler1/main.cpp
+++ ModernAssembler1/main.cpp
@@ -2,9 +2,12 @@
 
 using namespace std;
 
-extern "C" int SomeFunction();
-
+/// if you switch into x86 mode (32 bit) you can call assembly straight from c
 int main() {
-	cout << "The result is: " << SomeFunction() << "";
+	int i = 0;
+	_asm {
+		mov i, 56757
+	}
+	cout << "The result is: " << i << "";
 	return 0;
 }
